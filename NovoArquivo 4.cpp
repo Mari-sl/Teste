a54277e6003887049f6a1f8c5c076095fc51b1b2
Mari-sl <lima.marizete@aluno.ifsp.edu.br>
2025-05-14 15:29:06 -0300

    Update NovoArquivo 4.cpp

--- NovoArquivo 4.cpp
+++ NovoArquivo 4.cpp
@@ -6,12 +6,14 @@ int main()
 	double b;
 	double a;
 	double area;
+	
 	cout << "informe o valor da base do triangulo: ";
 	cin >> b;
 	cout << "informe o valor da altura do triangulo: ";
 	cin >> a;
 	area = ( b * a) / 2;
-	cout << "a area calculada do triangulo foi " << area;
+	cout << "a area calculada do triangulo foi: " << area << endl;
+	
 	return 0;
 	
 	
